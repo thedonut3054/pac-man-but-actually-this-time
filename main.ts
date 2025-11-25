d783d927d369c9d8c21771e14de8cc8a8a97de25
thedonut3054 <136283010+thedonut3054@users.noreply.github.com>
2025-11-24 23:14:27 -0500

    AI kind of good now

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ namespace SpriteKind {
     export const Coin = SpriteKind.create()
     export const Check = SpriteKind.create()
     export const fcheck = SpriteKind.create()
+    export const FrontPoint = SpriteKind.create()
+    export const bcheck = SpriteKind.create()
+    export const backPoint = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.tileKindAt(TileDirection.Top, assets.tile`myTile7`) && mySprite.vy != -80) {
@@ -13,8 +16,38 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         200,
         true
         )
+        Shoot_Forward()
+        Shoot_Backward()
     }
 })
+function Shoot_Backward () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.bcheck)
+    if (mySprite.vx != 0) {
+        if (mySprite.vx > 0) {
+            BackCheck = sprites.create(assets.image`myImage8`, SpriteKind.bcheck)
+            BackCheck.setFlag(SpriteFlag.Invisible, false)
+            BackCheck.setPosition(mySprite.x, mySprite.y)
+            BackCheck.setVelocity(-1000, 0)
+        } else if (mySprite.vx < 0) {
+            BackCheck = sprites.create(assets.image`myImage8`, SpriteKind.bcheck)
+            BackCheck.setFlag(SpriteFlag.Invisible, false)
+            BackCheck.setPosition(mySprite.x, mySprite.y)
+            BackCheck.setVelocity(1000, 0)
+        }
+    } else if (mySprite.vy != 0) {
+        if (mySprite.vy > 0) {
+            BackCheck = sprites.create(assets.image`myImage8`, SpriteKind.bcheck)
+            BackCheck.setFlag(SpriteFlag.Invisible, false)
+            BackCheck.setPosition(mySprite.x, mySprite.y)
+            BackCheck.setVelocity(0, -1000)
+        } else if (mySprite.vy < 0) {
+            BackCheck = sprites.create(assets.image`myImage8`, SpriteKind.bcheck)
+            BackCheck.setFlag(SpriteFlag.Invisible, false)
+            BackCheck.setPosition(mySprite.x, mySprite.y)
+            BackCheck.setVelocity(0, 1000)
+        }
+    }
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Coin, function (sprite, otherSprite) {
     sprites.destroy(otherSprite)
     info.changeScoreBy(10)
@@ -32,6 +65,8 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         200,
         true
         )
+        Shoot_Forward()
+        Shoot_Backward()
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -44,7 +79,25 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         200,
         true
         )
+        Shoot_Forward()
+        Shoot_Backward()
     }
+})
+scene.onHitWall(SpriteKind.bcheck, function (sprite, location) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.backPoint)
+    BackWaypoint = sprites.create(assets.image`myImage9`, SpriteKind.backPoint)
+    BackWaypoint.setFlag(SpriteFlag.Invisible, false)
+    BackWaypoint.setPosition(Math.round((sprite.x - 8) / 16) * 16 + 8, Math.round((sprite.y - 8) / 16) * 16 + 8)
+    sprites.destroy(sprite)
+    Ghost2.follow(BackWaypoint, 70)
+})
+scene.onHitWall(SpriteKind.fcheck, function (sprite, location) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.FrontPoint)
+    FrontWaypoint = sprites.create(assets.image`myImage6`, SpriteKind.FrontPoint)
+    FrontWaypoint.setFlag(SpriteFlag.Invisible, true)
+    FrontWaypoint.setPosition(Math.round((sprite.x - 8) / 16) * 16 + 8, Math.round((sprite.y - 8) / 16) * 16 + 8)
+    sprites.destroy(sprite)
+    Ghost.follow(FrontWaypoint, 70)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.tileKindAt(TileDirection.Bottom, assets.tile`myTile7`) && mySprite.vy != 80) {
@@ -56,11 +109,59 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         200,
         true
         )
+        Shoot_Forward()
+        Shoot_Backward()
     }
 })
+function Shoot_Forward () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.fcheck)
+    if (mySprite.vx != 0) {
+        if (mySprite.vx < 0) {
+            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
+            frontcheck.setFlag(SpriteFlag.Invisible, true)
+            frontcheck.setPosition(mySprite.x, mySprite.y)
+            frontcheck.setVelocity(-1000, 0)
+        } else if (mySprite.vx > 0) {
+            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
+            frontcheck.setFlag(SpriteFlag.Invisible, true)
+            frontcheck.setPosition(mySprite.x, mySprite.y)
+            frontcheck.setVelocity(1000, 0)
+        }
+    } else if (mySprite.vy != 0) {
+        if (mySprite.vy < 0) {
+            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
+            frontcheck.setFlag(SpriteFlag.Invisible, true)
+            frontcheck.setPosition(mySprite.x, mySprite.y)
+            frontcheck.setVelocity(0, -1000)
+        } else if (mySprite.vy > 0) {
+            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
+            frontcheck.setFlag(SpriteFlag.Invisible, true)
+            frontcheck.setPosition(mySprite.x, mySprite.y)
+            frontcheck.setVelocity(0, 1000)
+        }
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    timer.throttle("action", 500, function () {
+        mySprite.setVelocity(0, 0)
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`myAnim3`,
+        200,
+        false
+        )
+        mySprite.setPosition(11 * 16 + 8, 20 * 16 + 8)
+    })
+})
 let frontcheck: Sprite = null
+let FrontWaypoint: Sprite = null
+let BackWaypoint: Sprite = null
+let BackCheck: Sprite = null
+let Ghost2: Sprite = null
+let Ghost: Sprite = null
 let mySprite: Sprite = null
 info.setLife(3)
+game.setGameOverEffect(true, effects.none)
 namespace userconfig {
     export const ARCADE_SCREEN_WIDTH = 368
     export const ARCADE_SCREEN_HEIGHT = 368
@@ -69,26 +170,9 @@ tiles.setCurrentTilemap(tilemap`level`)
 tileUtil.createSpritesOnTiles(assets.tile`myTile7`, assets.image`myImage`, SpriteKind.Coin)
 mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Player)
 mySprite.setPosition(11 * 16 + 8, 20 * 16 + 8)
-game.onUpdateInterval(500, function () {
-    if (mySprite.vx != 0) {
-        if (mySprite.vx < 0) {
-            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
-            frontcheck.setPosition(mySprite.x, mySprite.y)
-            frontcheck.setVelocity(-100, 0)
-        } else if (mySprite.vx > 0) {
-            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
-            frontcheck.setPosition(mySprite.x, mySprite.y)
-            frontcheck.setVelocity(100, 0)
-        }
-    } else if (mySprite.vy != 0) {
-        if (mySprite.vy < 0) {
-            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
-            frontcheck.setPosition(mySprite.x, mySprite.y)
-            frontcheck.setVelocity(0, -100)
-        } else if (mySprite.vy > 0) {
-            frontcheck = sprites.create(assets.image`myImage5`, SpriteKind.fcheck)
-            frontcheck.setPosition(mySprite.x, mySprite.y)
-            frontcheck.setVelocity(0, 100)
-        }
-    }
-})
+Ghost = sprites.create(assets.image`myImage7`, SpriteKind.Enemy)
+Ghost.setPosition(9 * 16 + 8, 15 * 16 + 8)
+Ghost.follow(mySprite, 70)
+Ghost2 = sprites.create(assets.image`myImage10`, SpriteKind.Enemy)
+Ghost2.setPosition(11 * 16 + 8, 15 * 16 + 8)
+Ghost.follow(mySprite, 70)
